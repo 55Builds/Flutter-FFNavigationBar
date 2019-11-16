1d1549879491c2b56acd333f7baa315b89a54ddf
Jon WB <jon@e-nablesolutions.com>
2019-11-16 22:53:43 +1100

    Added example code

---
+++ example/ff_nagivation_bar_example/lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:ff_navigation_bar/ff_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ff_navigation_bar Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'ff_navigation_bar Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Demonstration',
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: FFNavigationBar(
+        theme: FFNavigationBarTheme(
+          barBackgroundColor: Colors.white,
+          selectedItemBackgroundColor: Colors.green,
+          selectedItemIconColor: Colors.white,
+          selectedItemLabelColor: Colors.black,
+        ),
+        selectedIndex: selectedIndex,
+        onSelectTab: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        items: [
+          FFNavigationBarItem(
+            iconData: Icons.calendar_today,
+            label: 'Bar Theme',
+          ),
+          FFNavigationBarItem(
+            iconData: Icons.people,
+            label: 'Orange',
+            selectedBackgroundColor: Colors.orange,
+          ),
+          FFNavigationBarItem(
+            iconData: Icons.attach_money,
+            label: 'Purple',
+            selectedBackgroundColor: Colors.purple,
+          ),
+          FFNavigationBarItem(
+            iconData: Icons.note,
+            label: 'Blue',
+            selectedBackgroundColor: Colors.blue,
+          ),
+          FFNavigationBarItem(
+            iconData: Icons.settings,
+            label: 'Red Item',
+            selectedBackgroundColor: Colors.red,
+          ),
+        ],
+      ),
+    );
+  }
+}
