396a733552c54cda31f85deaff9793052483aaac
Jon WB <jon@e-nablesolutions.com>
2019-11-27 13:10:40 +1100

    Provide support for 4.0.0-dev release of Provider
    Requires Flutter version > 1.12.1

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/jwb/Local/Workspaces/Unversioned/FlutterDevelopment"
-export "FLUTTER_APPLICATION_PATH=/Users/jwb/Local/Workspaces/Repositories/Flutter-FFNavigationBar/example/ff_nagivation_bar_example"
-export "FLUTTER_TARGET=/Users/jwb/Local/Workspaces/Repositories/Flutter-FFNavigationBar/example/ff_nagivation_bar_example/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/jwb/Local/Workspaces/Repositories/Flutter-FFNavigationBar/example"
+export "FLUTTER_TARGET=/Users/jwb/Local/Workspaces/Repositories/Flutter-FFNavigationBar/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/jwb/Local/Workspaces/Unversioned/FlutterDevelopment/bin/cache/artifacts/engine/ios"
