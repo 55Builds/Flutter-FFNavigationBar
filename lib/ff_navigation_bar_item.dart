99d1705f9e154a9fa99f88f5c130b4f3b214298a
Mustafa Ersoy <40203872+mustafaersoyer@users.noreply.github.com>
2020-12-23 14:17:09 +0300

    decrease selected item offset

--- lib/ff_navigation_bar_item.dart
+++ lib/ff_navigation_bar_item.dart
@@ -180,7 +180,7 @@ class FFNavigationBarItem extends StatelessWidget {
               ),
             ),
             Positioned(
-              top: topOffset,
+              top: isSelected ? topOffset - 2.5 : topOffset,
               left: -itemWidth / 2,
               right: -itemWidth / 2,
               child: Column(
